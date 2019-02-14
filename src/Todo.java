42ac50ea741d55fe4fd25312da4c986ffbed032d
bradenmunn <bradenmunn@yahoo.com>
2019-02-14 11:54:42 -0600

    Began lab, started todo.java tracking and began work on parseAndExecute

---
+++ src/Todo.java
@@ -0,0 +1,18 @@
+public class Todo {/*
+Lab Section_________: 13
+Last Modified_______: 2019-02-14
+OU ID_______________: 113483760
+OU EMAIL____________: bradenmunn@ou.edu
+
+Tasks:
+1. Implement the CalculateTwoTokens method
+2. Parse the String in parseAndExecute
+
+Estimated Time (minutes):
+1. 15
+2. 10
+
+Actual Time (minutes):
+1. XXX
+2. XXX
+*/}
